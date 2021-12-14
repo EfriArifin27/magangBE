6d2c3393dc5d6f341ebbd8e5698f573d8882aab5
Efri Triana Nur Arifin <efritriana27@gmail.com>
2021-12-15 00:22:56 +0700

    awal hari ketiga

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true })
+const fastify = require('fastify')({ logger: true });
+const path = require("path")
+
+// Register plugins
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public/assets'),
+  prefix: "/", // optional: default '/'
+})
+
+// Declare a route
+fastify.get('/another/path', function (req, reply) {
+  return reply.sendFile("index.html") // serving path.join(__dirname, 'public', 'index.html') directly
+})
 
 // Declare a route
 fastify.get('/', async (request, reply) => {
